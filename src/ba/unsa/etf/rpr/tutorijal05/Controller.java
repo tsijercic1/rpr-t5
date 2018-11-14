a49eeada76ef88ff3084187ebe9398de2a1ce86b
tsijercic1 <tsijercic1@etf.unsa.ba>
2018-11-14 10:56:28 +0100

    Started with Controller implementation

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,14 +1,17 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    public Label display;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        display.setText("0");
     }
 }
