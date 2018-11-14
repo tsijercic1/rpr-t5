7d3eaf6b9db1a67ed3fd9aacf6d68bde987ede96
tsijercic1 <tsijercic1@etf.unsa.ba>
2018-11-14 11:57:43 +0100

    Implementing function of the buttons

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,6 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
@@ -9,9 +10,92 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     public Label display;
+    private String result="0";
+    private String operand="";
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         display.setText("0");
+    }
+
+    public void zero(ActionEvent actionEvent) {
+        if(!operand.equals("")){
+            operand+="0";
+            display.setText(operand);
+        }
+    }
+
+    public void one(ActionEvent actionEvent) {
+        operand+="1";
+        display.setText(operand);
+    }
+
+    public void nine(ActionEvent actionEvent) {
+        operand+="9";
+        display.setText(operand);
+    }
+
+
+    public void seven(ActionEvent actionEvent) {
+        operand+="7";
+        display.setText(operand);
+    }
+
+    public void six(ActionEvent actionEvent) {
+        operand+="6";
+        display.setText(operand);
+    }
+
+    public void three(ActionEvent actionEvent) {
+        operand+="3";
+        display.setText(operand);
+    }
+
+    public void five(ActionEvent actionEvent) {
+        operand+="5";
+        display.setText(operand);
+    }
+
+    public void four(ActionEvent actionEvent) {
+        operand+="4";
+        display.setText(operand);
+    }
+
+    public void eight(ActionEvent actionEvent) {
+        operand+="8";
+        display.setText(operand);
+    }
+
+    public void two(ActionEvent actionEvent) {
+        operand+="2";
+        display.setText(operand);
+    }
+
+    public void plus(ActionEvent actionEvent) {
+    }
+
+    public void minus(ActionEvent actionEvent) {
+
+    }
+
+    public void divide(ActionEvent actionEvent) {
+    }
+
+    public void mod(ActionEvent actionEvent) {
+    }
+
+    public void times(ActionEvent actionEvent) {
+    }
+
+    public void dot(ActionEvent actionEvent) {
+        String temp=operand;
+        if(!operand.matches(".")){
+            operand+=".";
+            display.setText(operand);
+        }
+    }
+
+    public void equals(ActionEvent actionEvent) {
+
     }
 }
